024675ae60b60ae12f4d7b7ea78427e5b693a86b
AltimorTASDK <altimormc@gmail.com>
2016-09-30 13:50:55 -0400

    made the thing

---
+++ ffxiv_fov/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ffxiv_fov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.fovInput = new System.Windows.Forms.TextBox();
+			this.useHorFov = new System.Windows.Forms.RadioButton();
+			this.useVertFov = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.statusBox = new System.Windows.Forms.TextBox();
+			this.setFov = new System.Windows.Forms.Button();
+			this.fovDisplay = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 86);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Current FOV";
+			// 
+			// fovInput
+			// 
+			this.fovInput.Location = new System.Drawing.Point(84, 109);
+			this.fovInput.Name = "fovInput";
+			this.fovInput.Size = new System.Drawing.Size(77, 20);
+			this.fovInput.TabIndex = 6;
+			// 
+			// useHorFov
+			// 
+			this.useHorFov.AutoSize = true;
+			this.useHorFov.Checked = true;
+			this.useHorFov.Location = new System.Drawing.Point(6, 19);
+			this.useHorFov.Name = "useHorFov";
+			this.useHorFov.Size = new System.Drawing.Size(48, 17);
+			this.useHorFov.TabIndex = 9;
+			this.useHorFov.TabStop = true;
+			this.useHorFov.Text = "Hor+";
+			this.useHorFov.UseVisualStyleBackColor = true;
+			// 
+			// useVertFov
+			// 
+			this.useVertFov.AutoSize = true;
+			this.useVertFov.Location = new System.Drawing.Point(6, 42);
+			this.useVertFov.Name = "useVertFov";
+			this.useVertFov.Size = new System.Drawing.Size(60, 17);
+			this.useVertFov.TabIndex = 10;
+			this.useVertFov.Text = "Vertical";
+			this.useVertFov.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.useHorFov);
+			this.groupBox1.Controls.Add(this.useVertFov);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(149, 68);
+			this.groupBox1.TabIndex = 11;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Mode";
+			// 
+			// statusBox
+			// 
+			this.statusBox.Location = new System.Drawing.Point(167, 12);
+			this.statusBox.Multiline = true;
+			this.statusBox.Name = "statusBox";
+			this.statusBox.ReadOnly = true;
+			this.statusBox.Size = new System.Drawing.Size(250, 117);
+			this.statusBox.TabIndex = 12;
+			// 
+			// setFov
+			// 
+			this.setFov.Location = new System.Drawing.Point(10, 109);
+			this.setFov.Name = "setFov";
+			this.setFov.Size = new System.Drawing.Size(62, 20);
+			this.setFov.TabIndex = 13;
+			this.setFov.Text = "Set FOV";
+			this.setFov.UseVisualStyleBackColor = true;
+			this.setFov.Click += new System.EventHandler(this.setFov_Click);
+			// 
+			// fovDisplay
+			// 
+			this.fovDisplay.Location = new System.Drawing.Point(84, 83);
+			this.fovDisplay.Name = "fovDisplay";
+			this.fovDisplay.ReadOnly = true;
+			this.fovDisplay.Size = new System.Drawing.Size(77, 20);
+			this.fovDisplay.TabIndex = 14;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(429, 141);
+			this.Controls.Add(this.fovDisplay);
+			this.Controls.Add(this.setFov);
+			this.Controls.Add(this.statusBox);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.fovInput);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "FFXIV Field of View";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fovInput;
+        private System.Windows.Forms.RadioButton useHorFov;
+        private System.Windows.Forms.RadioButton useVertFov;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox statusBox;
+		private System.Windows.Forms.Button setFov;
+		private System.Windows.Forms.TextBox fovDisplay;
+    }
+}
+
---
+++ ffxiv_fov/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ffxiv_fov
+{
+	public partial class Form1 : Form
+	{
+		[Flags]
+		public enum ProcessAccessFlags : uint
+		{
+			All = 0x001F0FFF,
+			Terminate = 0x00000001,
+			CreateThread = 0x00000002,
+			VirtualMemoryOperation = 0x00000008,
+			VirtualMemoryRead = 0x00000010,
+			VirtualMemoryWrite = 0x00000020,
+			DuplicateHandle = 0x00000040,
+			CreateProcess = 0x000000080,
+			SetQuota = 0x00000100,
+			SetInformation = 0x00000200,
+			QueryInformation = 0x00000400,
+			QueryLimitedInformation = 0x00001000,
+			Synchronize = 0x00100000
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern IntPtr OpenProcess(
+			 ProcessAccessFlags processAccess,
+			 bool bInheritHandle,
+			 int processId);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		static extern bool ReadProcessMemory(
+			IntPtr hProcess,
+			IntPtr lpBaseAddress,
+			[Out] byte[] lpBuffer,
+			int dwSize,
+			out IntPtr lpNumberOfBytesRead);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		static extern bool WriteProcessMemory(
+			IntPtr hProcess,
+			IntPtr lpBaseAddress,
+			byte[] lpBuffer,
+			int nSize,
+			out IntPtr lpNumberOfBytesWritten);
+
+		private Thread mainThread = null;
+
+		private bool dx9WarningShown = false;
+
+		private Process ffxiv = null;
+		private IntPtr ffxivHandle = IntPtr.Zero;
+		private IntPtr cameraOffset = IntPtr.Zero;
+		// Indicates that the above variables have been set
+		private bool ffxivFound = false;
+
+		private static byte[] cameraPattern = new byte[] { 0x48, 0x8B, 0x1C, 0xC3, 0x48, 0x85, 0xDB, 0x0F, 0x84 };
+
+		private const int fovOffset = 0x124;
+		private const int minFovOffset = 0x128;
+		private const int maxFovOffset = 0x12C;
+
+		// Terminate main loop
+		private bool exitLoop = false;
+
+		delegate void SetTextCallback(string text);
+		private void Message(string text)
+		{
+			if (statusBox.InvokeRequired)
+			{
+				var d = new SetTextCallback(Message);
+				Invoke(d, new object[] { text });
+			}
+			else
+			{
+				var newText = statusBox.Text + text + Environment.NewLine;
+				statusBox.Text = newText;
+			}
+		}
+
+		private void ShowFOV(string text)
+		{
+			if (fovDisplay.InvokeRequired)
+			{
+				var d = new SetTextCallback(ShowFOV);
+				Invoke(d, new object[] { text });
+			}
+			else
+			{
+				fovDisplay.Text = text;
+			}
+		}
+
+		private void Reset()
+		{
+			fovInput.Text = "";
+			Message("Waiting for FFXIV");
+			ffxiv = null;
+			ffxivHandle = IntPtr.Zero;
+			ffxivFound = false;
+		}
+
+		private static IntPtr PatternScan(IntPtr handle, IntPtr start, int size, byte[] pattern)
+		{
+			var buf = new byte[pattern.Length];
+			for (var addr = start; addr != (IntPtr)(size) - pattern.Length + 1; addr += 1)
+			{
+				IntPtr bytesRead;
+				if (ReadProcessMemory(handle, addr, buf, pattern.Length, out bytesRead) && buf.SequenceEqual(pattern))
+					return addr;
+			}
+
+			return IntPtr.Zero;
+		}
+
+		private delegate T ConverterType<T>(byte[] buf, int startIndex);
+		private static bool ReadType<T>(IntPtr handle, IntPtr addr, ConverterType<T> converter, out T result)
+		{
+			IntPtr bytesRead;
+			var size = Marshal.SizeOf(typeof(T));
+			var buf = new byte[size];
+			if (!ReadProcessMemory(handle, addr, buf, size, out bytesRead))
+			{
+				result = default(T);
+				return false;
+			}
+
+			result = converter(buf, 0);
+			return true;
+		}
+
+		private static bool WriteType<T>(IntPtr handle, IntPtr addr, T value)
+		{
+			IntPtr bytesRead;
+
+			// Convert to byte buffer
+			var size = Marshal.SizeOf(typeof(T));
+			var buf = new byte[size];
+			var gcHandle = GCHandle.Alloc(value, GCHandleType.Pinned);
+			Marshal.Copy(gcHandle.AddrOfPinnedObject(), buf, 0, size);
+
+			return WriteProcessMemory(handle, addr, buf, size, out bytesRead);
+		}
+
+		// Convert FOV from the set display format to vertical+radians
+		private float ConvertFOVFromDisplay(float value)
+		{
+			if (useHorFov.Checked)
+				return (float)(Math.Atan(Math.Tan(value * Math.PI / 180.0 / 2.0) * 3.0 / 4.0) * 2.0);
+			else
+				return (float)(value * Math.PI / 180.0);
+		}
+
+		// Convert FOV from vertical+radians to the set display format
+		private float ConvertFOVToDisplay(float value)
+		{
+			if (useHorFov.Checked)
+				return (float)(Math.Atan(Math.Tan(value / 2.0) * 4.0 / 3.0) * 2.0 * 180.0 / Math.PI);
+			else
+				return (float)(value * 180.0 / Math.PI);
+		}
+
+		private void UpdateFOV()
+		{
+			Int64 camera;
+			if (!ReadType<Int64>(ffxivHandle, cameraOffset, BitConverter.ToInt64, out camera))
+			{
+				Message("Invalid camera pointer address");
+				exitLoop = true;
+				return;
+			}
+
+			if (camera == 0)
+				return;
+
+			float maxFov;
+			ReadType<float>(ffxivHandle, (IntPtr)(camera + maxFovOffset), BitConverter.ToSingle, out maxFov);
+
+			ShowFOV(ConvertFOVToDisplay(maxFov).ToString());
+		}
+
+		private void setFov_Click(object sender, EventArgs e)
+		{
+			if (exitLoop || !ffxivFound)
+				return;
+
+			float newMaxFov;
+			if (!float.TryParse(fovInput.Text, out newMaxFov))
+				return;
+
+			newMaxFov = ConvertFOVFromDisplay(newMaxFov);
+
+			// Sanity check
+			if (newMaxFov <= 0F || newMaxFov >= Math.PI)
+				return;
+
+			Int64 camera;
+			if (!ReadType<Int64>(ffxivHandle, cameraOffset, BitConverter.ToInt64, out camera))
+			{
+				Message("Invalid camera pointer address");
+				exitLoop = true;
+				return;
+			}
+
+			if (camera == 0)
+				return;
+
+			float fov, minFov, maxFov;
+			ReadType<float>(ffxivHandle, (IntPtr)(camera + fovOffset), BitConverter.ToSingle, out fov);
+			ReadType<float>(ffxivHandle, (IntPtr)(camera + minFovOffset), BitConverter.ToSingle, out minFov);
+			ReadType<float>(ffxivHandle, (IntPtr)(camera + maxFovOffset), BitConverter.ToSingle, out maxFov);
+
+			var fovTan = Math.Tan(fov / 2.0);
+			var minFovTan = Math.Tan(minFov / 2.0);
+			var maxFovTan = Math.Tan(maxFov / 2.0);
+			var newMaxFovTan = Math.Tan(newMaxFov / 2.0);
+			var newMinFovTan = newMaxFovTan * minFovTan / maxFovTan;
+
+			// Re-lerp for new max and min FOV
+			var zoomPct = (fovTan - minFovTan) / (maxFovTan - minFovTan);
+			var newFovTan = newMinFovTan + (newMaxFovTan - newMinFovTan) * zoomPct;
+
+			var newMinFov = (float)(Math.Atan(newMinFovTan) * 2.0);
+			var newFov = (float)(Math.Atan(newFovTan) * 2.0);
+
+			WriteType<float>(ffxivHandle, (IntPtr)(camera + fovOffset), newFov);
+			WriteType<float>(ffxivHandle, (IntPtr)(camera + minFovOffset), newMinFov);
+			WriteType<float>(ffxivHandle, (IntPtr)(camera + maxFovOffset), newMaxFov);
+		}
+
+		private void ScanForFFXIV()
+		{
+			if (!dx9WarningShown && Process.GetProcessesByName("ffxiv").Length > 0)
+			{
+				Message("DX9 client is not supported");
+				dx9WarningShown = true;
+			}
+
+			var processes = Process.GetProcessesByName("ffxiv_dx11");
+			if (processes.Length == 0)
+				return;
+
+			ffxiv = processes[0];
+			Message("Found FFXIV (PID: " + processes[0].Id + ")");
+
+			const ProcessAccessFlags flags =
+				ProcessAccessFlags.VirtualMemoryOperation |
+				ProcessAccessFlags.VirtualMemoryRead |
+				ProcessAccessFlags.VirtualMemoryWrite;
+
+			ffxivHandle = OpenProcess(flags, false, ffxiv.Id);
+			if(ffxivHandle == IntPtr.Zero)
+			{
+				Message("Failed to open handle");
+				exitLoop = true;
+				return;
+			}
+
+			var cameraRef = PatternScan(ffxivHandle, ffxiv.MainModule.BaseAddress, ffxiv.MainModule.ModuleMemorySize, cameraPattern);
+			if (cameraRef == IntPtr.Zero)
+			{
+				Message("Pattern scan failed");
+				exitLoop = true;
+				return;
+			}
+
+			Int32 leaOffset;
+			if (!ReadType<Int32>(ffxivHandle, cameraRef - 4, BitConverter.ToInt32, out leaOffset))
+			{
+				Message("Invalid camera pointer reference address");
+				exitLoop = true;
+				return;
+			}
+
+			cameraOffset = cameraRef + leaOffset;
+
+			ffxivFound = true;
+		}
+
+		public void MainLoop()
+		{
+			while (!exitLoop)
+			{
+				if (ffxivFound && ffxiv.HasExited)
+					Reset();
+
+				if (!ffxivFound)
+					ScanForFFXIV();
+
+				if (ffxivFound)
+					UpdateFOV();
+
+				Thread.Sleep(1000);
+			}
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Reset();
+			mainThread = new Thread(MainLoop);
+			mainThread.Start();
+		}
+
+		private void Form1_FormClosed(object sender, EventArgs e)
+		{
+			exitLoop = true;
+		}
+	}
+}
